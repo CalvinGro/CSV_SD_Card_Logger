ba33a6973d447fe6f44bc1a5a28336237804c8ef
CalvinGro <153310569+CalvinGro@users.noreply.github.com>
2026-08-15 10:59:10 -0500

    Finished CRC16 and Get Sector Count functions

--- src/sd_card.c
+++ src/sd_card.c
@@ -15,6 +15,8 @@ Description - In this file I abstract away the SD Card protocols. This handles
 
 /** Calculate the Cyclic Redundancy Check (CRC) for any 16-bit pattern. 
     Adds the extra 15 bits to the end for building a crc to send.
+
+    Returns the final two byte frame check sequence (FCS) in the response_adr.
  */
 HAL_StatusTypeDef Calc_CRC16(
     uint16_t pattern, 
@@ -23,27 +25,29 @@ HAL_StatusTypeDef Calc_CRC16(
     uint16_t *response_adr, 
     bool building_crc
     ) {
-        if (byte_count < 2 || response_adr == NULL || data == NULL || (pattern & 0x80 != 0x80)) {
+        if (byte_count < 2 || response_adr == NULL || data == NULL) {
             return HAL_ERROR;
         }
         uint16_t cur_data = (data[0] << 8) | data[1];
         for (uint32_t i = 2; i < byte_count; i++) {
             for (uint8_t j = 0; j < 8; j++) {
-                bool bit15_set = (cur_data & 0x8000) == 0x8000;
-                cur_data = cur_data << 1 | (data[i] & (uint16_t)1 << j);
-                if (bit15_set) {
+                bool bit16_set = (cur_data & 0x8000) == 0x8000;
+                // Send each byte MSB first, so (7-j) flips the order.
+                cur_data = (uint16_t)((cur_data << 1) | ((data[i] & (uint16_t)1 << (7 - j)) >> (7 - j)));
+                if (bit16_set) {
                     cur_data ^= pattern;
                 }
             }
         }
 
         if (building_crc) {
-            for (uint8_t i = 0; i < 8; i++) {
-                if (cur_data & 0x8000 == 0x8000) {
+            for (uint8_t i = 0; i < 16; i++) {
+                bool bit16_set = (cur_data & 0x8000) == 0x8000;
+                cur_data = (uint16_t)(cur_data << 1);
+
+                if (bit16_set) {
                     cur_data ^= pattern;
                 }
-
-                cur_data <<= 1;
             }
         }
         *response_adr = cur_data;
@@ -134,9 +138,9 @@ HAL_StatusTypeDef Send_Command(Card_Handle *card, uint8_t cmd, uint32_t cmd_arg,
     command_packet[4] = (uint8_t)data;
     command_packet[5] = crc7_byte;
 
-    // transmission loop, this can be repeated up to 8 times in the case of crc errors.
+    // transmission loop, this can be repeated up to 3 times in the case of crc errors.
     uint8_t attempt_count = 0;
-    while (attempt_count < 8) {
+    while (attempt_count < 3) {
         status = Wait_Till_Ready(card);
         if (status != HAL_OK) return status;
 
@@ -159,63 +163,84 @@ HAL_StatusTypeDef Send_Command(Card_Handle *card, uint8_t cmd, uint32_t cmd_arg,
 
         attempt_count++;
     }
-    if (attempt_count == 8) return HAL_TIMEOUT;
 
     *R1_adr = R1_response;
+
+    if (attempt_count == 3) return HAL_ERROR;
     return HAL_OK;
 }
 
-
-HAL_StatusTypeDef Get_Sector_Count(Card_Handle *card) {
-    if (card == NULL || card->spi_handle == NULL) {
+/** Calculate the number of sectors in the current card.
+ */
+HAL_StatusTypeDef Get_Sector_Count(Card_Handle *card, uint64_t *sector_count_adr) {
+    if (card == NULL || card->spi_handle == NULL || sector_count_adr == NULL) {
         return HAL_ERROR;
     }
 
+    HAL_StatusTypeDef status;
+
     status = SD_SPI_Select_Card(card->spi_handle);
-    if (status != HAL_OK) return status;
+    if (status != HAL_OK) goto cleanup;
 
-    uint8_t cmd9 = 0x49;
-    uint8_t cmd9_arg[4];
-    uint8_t R1_response;
-    memset(cmd9_arg, 0, sizeof(cmd9_arg));
-
-    status = Send_Command(card, cmd9, cmd9_arg, &R1_response);
-    if (status != HAL_OK) return status;
-    if (R1_response != 0) return HAL_ERROR;
-
-    // wait till 0xFE byte to signal the start of data transfers.
-    status = Wait_To_Receive(card);
-    if (status != HAL_OK) return status;
-
+    // retrieve CSD register
+    uint8_t attempt_count = 0;
+    uint16_t response;
     uint8_t CSD_reg[18];
+    do {
+        uint8_t cmd9 = 0x49;
+        uint32_t cmd9_arg = 0;
+        uint8_t R1_response;
 
-    status = SD_SPI_Receive_Data(card->spi_handle, CSD_reg, sizeof(CSD_reg));
-    if (status != HAL_OK) return status;
-
-
-    // check the crc16 
-    // calculate crc7 using generator polynomial (pattern).
-    uint16_t crc16_bytes;
-    uint64_t data = (((uint64_t)cmd << 32) | (uint64_t)cmd_arg);
-    uint64_t pattern = (uint64_t)0x89 << 39; // divisor 
-    uint64_t data_dividend = data << 7;
-    for (uint8_t i = 46; i >= 7; i--) {
-        if ((data_dividend & ((uint64_t)1 << i)) != 0) {
-            data_dividend ^= pattern;
+        status = Send_Command(card, cmd9, cmd9_arg, &R1_response);
+        if (status != HAL_OK) goto cleanup;
+        if (R1_response != 0) {
+            status = HAL_ERROR;
+            goto cleanup;
         }
-        pattern >>= 1;
-    } 
-    crc7_byte = ((uint8_t)(data_dividend & (uint64_t)0x7f) << 1) | 1;
+
+        // wait till 0xFE byte to signal the start of data transfers.
+        status = Wait_To_Receive(card);
+        if (status != HAL_OK) goto cleanup;
 
 
+        status = SD_SPI_Receive_Data(card->spi_handle, CSD_reg, sizeof(CSD_reg));
+        if (status != HAL_OK) goto cleanup;
+
+
+        // check the crc16
+        uint16_t pattern = 0x1021; 
+        
+        status = Calc_CRC16(pattern, CSD_reg, sizeof(CSD_reg), &response, false);
+        if (status != HAL_OK) goto cleanup;
+
+        attempt_count++;
+    } while (response != 0 && attempt_count < 3);
+    if (response != 0) {
+        status = HAL_ERROR;
+        goto cleanup;
+    }
 
     // verify that it is a High Capacity or Extended Capacity Card. 
     if ((CSD_reg[0] & 0xc0) != 0x40) {
-        return HAL_ERROR;
+        status = HAL_ERROR;
+        goto cleanup;
     }
 
-    uint32_t c_size = (((uint8_t)CSD_reg[7] & 0x3f) << 16) | ((uint8_t)CSD_reg[8] << 8) | (uint8_t)CSD_reg[9];
+    // Find the c_size from the CSD register.
+    uint64_t c_size = (((uint64_t)CSD_reg[7] & 0x3f) << 16) | ((uint64_t)CSD_reg[8] << 8) | (uint64_t)CSD_reg[9];
 
+    *sector_count_adr = (c_size + 1) * 1024;
+
+    status = HAL_OK;
+
+cleanup:
+    (void)SD_SPI_Deselect_Card(card->spi_handle);
+
+    // transmit 0xff byte to provide trailing clocks after CS deselects the chip to reset SPI.
+    uint8_t filler_response;
+    (void)SD_SPI_Interchange_Byte(card->spi_handle, 0xff, &filler_response);
+
+    return status;
 }
 
 
