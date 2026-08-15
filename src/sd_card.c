2cfaf13f6b617334598fc76ea4a82fa8b7ecbd07
CalvinGro <153310569+CalvinGro@users.noreply.github.com>
2026-08-14 21:52:41 -0500

    finished send command function

--- src/sd_card.c
+++ src/sd_card.c
@@ -10,10 +10,53 @@ Description - In this file I abstract away the SD Card protocols. This handles
 #include "sd_spi.h"
 #include "sd_card.h"
 #include <string.h>
+#include <stdbool.h>
 
 
+/** Calculate the Cyclic Redundancy Check (CRC) for any 16-bit pattern. 
+    Adds the extra 15 bits to the end for building a crc to send.
+ */
+HAL_StatusTypeDef Calc_CRC16(
+    uint16_t pattern, 
+    const uint8_t *data, 
+    uint32_t byte_count, 
+    uint16_t *response_adr, 
+    bool building_crc
+    ) {
+        if (byte_count < 2 || response_adr == NULL || data == NULL || (pattern & 0x80 != 0x80)) {
+            return HAL_ERROR;
+        }
+        uint16_t cur_data = (data[0] << 8) | data[1];
+        for (uint32_t i = 2; i < byte_count; i++) {
+            for (uint8_t j = 0; j < 8; j++) {
+                bool bit15_set = (cur_data & 0x8000) == 0x8000;
+                cur_data = cur_data << 1 | (data[i] & (uint16_t)1 << j);
+                if (bit15_set) {
+                    cur_data ^= pattern;
+                }
+            }
+        }
 
-HAL_StatusTypeDef Wait_Ready(Card_Handle *card) {
+        if (building_crc) {
+            for (uint8_t i = 0; i < 8; i++) {
+                if (cur_data & 0x8000 == 0x8000) {
+                    cur_data ^= pattern;
+                }
+
+                cur_data <<= 1;
+            }
+        }
+        *response_adr = cur_data;
+
+        return HAL_OK;
+}
+
+
+/** This function waits until the card is ready to communicate over SPI.
+    It assumes that the card is already selected.
+ */
+HAL_StatusTypeDef Wait_Till_Ready(Card_Handle *card) {
+    if (card == NULL || card->spi_handle == NULL) return HAL_ERROR;
 
     HAL_StatusTypeDef status;
     uint8_t received_byte;
@@ -21,11 +64,28 @@ HAL_StatusTypeDef Wait_Ready(Card_Handle *card) {
     do {
         status = SD_SPI_Interchange_Byte(card->spi_handle, 0xff, &received_byte);
         if (status != HAL_OK) return status;
-    } while ((HAL_GetTick() - start_time < 100) && received_byte == 0x00);
+    } while ((HAL_GetTick() - start_time < 300) && received_byte != 0xff);
 
-    if (received_byte == 0xff) {
-        return HAL_OK;
-    }
+    if (received_byte == 0xff) return HAL_OK;
+    return HAL_TIMEOUT;
+}
+
+
+HAL_StatusTypeDef Wait_To_Receive(Card_Handle *card) {
+    if (card == NULL || card->spi_handle == NULL) return HAL_ERROR;
+
+    HAL_StatusTypeDef status;
+    uint8_t received_byte;
+    uint32_t start_time = HAL_GetTick();
+    do {
+        status = SD_SPI_Interchange_Byte(card->spi_handle, 0xff, &received_byte);
+        if (status != HAL_OK) return status;
+        if ((received_byte != 0xff) && (received_byte != 0xfe)) {
+            return HAL_ERROR;
+        }
+    } while ((HAL_GetTick() - start_time < 300) && received_byte != 0xfe);
+
+    if (received_byte == 0xff) return HAL_OK;
     return HAL_TIMEOUT;
 }
 
@@ -35,11 +95,129 @@ HAL_StatusTypeDef Wait_Ready(Card_Handle *card) {
     argument, then the final byte is the crc, which represents the end of the command
     and is calculated based off the command and its argument.
 
-    The card should already be selected before sending commands
+    The card should already be selected before sending commands.
+
+    Additionally, this function handles the R1 response, but all other responses should
+    be handled by the caller.
  */
 HAL_StatusTypeDef Send_Command(Card_Handle *card, uint8_t cmd, uint32_t cmd_arg, uint8_t *R1_adr) {
-    
+    if (card == NULL || card->spi_handle == NULL || R1_adr == NULL) {
+        return HAL_ERROR;
+    }
+
+    // set start bit to 0 and transmission bit to host-to-card (1).
+    cmd &= 0x7f;
+    cmd |= 0x40;
+
+    HAL_StatusTypeDef status;
+    uint8_t R1_response;
+
+    // calculate crc7 using generator polynomial (pattern).
+    uint8_t crc7_byte;
+    uint64_t data = (((uint64_t)cmd << 32) | (uint64_t)cmd_arg);
+    uint64_t pattern = (uint64_t)0x89 << 39; // divisor 
+    uint64_t data_dividend = data << 7;
+    for (uint8_t i = 46; i >= 7; i--) {
+        if ((data_dividend & ((uint64_t)1 << i)) != 0) {
+            data_dividend ^= pattern;
+        }
+        pattern >>= 1;
+    } 
+    crc7_byte = ((uint8_t)(data_dividend & (uint64_t)0x7f) << 1) | 1;
+
+    //
+    uint8_t command_packet[6];
+    command_packet[0] = cmd;
+    command_packet[1] = (uint8_t)(data >> 24);
+    command_packet[2] = (uint8_t)(data >> 16);
+    command_packet[3] = (uint8_t)(data >> 8);
+    command_packet[4] = (uint8_t)data;
+    command_packet[5] = crc7_byte;
+
+    // transmission loop, this can be repeated up to 8 times in the case of crc errors.
+    uint8_t attempt_count = 0;
+    while (attempt_count < 8) {
+        status = Wait_Till_Ready(card);
+        if (status != HAL_OK) return status;
+
+        // send the cmd packet to the card
+        status = SD_SPI_Transmit_Data(card->spi_handle, command_packet, sizeof(command_packet));
+        if (status != HAL_OK) return status;
+        
+        // listen for R1 response
+        uint32_t start_time = HAL_GetTick();
+        do {
+            status = SD_SPI_Interchange_Byte(card->spi_handle, 0xff, &R1_response);
+            if (status != HAL_OK) return status;
+        } while ((HAL_GetTick() - start_time < 50) && (R1_response & 0x80) == 0x80);
+        if ((R1_response & 0x80) == 0x80) return HAL_TIMEOUT;
+
+        // if no crc error don't repeat the transmission
+        if ((R1_response & 0x08) != 0x08) {
+            break;
+        }
+
+        attempt_count++;
+    }
+    if (attempt_count == 8) return HAL_TIMEOUT;
+
+    *R1_adr = R1_response;
+    return HAL_OK;
 }
+
+
+HAL_StatusTypeDef Get_Sector_Count(Card_Handle *card) {
+    if (card == NULL || card->spi_handle == NULL) {
+        return HAL_ERROR;
+    }
+
+    status = SD_SPI_Select_Card(card->spi_handle);
+    if (status != HAL_OK) return status;
+
+    uint8_t cmd9 = 0x49;
+    uint8_t cmd9_arg[4];
+    uint8_t R1_response;
+    memset(cmd9_arg, 0, sizeof(cmd9_arg));
+
+    status = Send_Command(card, cmd9, cmd9_arg, &R1_response);
+    if (status != HAL_OK) return status;
+    if (R1_response != 0) return HAL_ERROR;
+
+    // wait till 0xFE byte to signal the start of data transfers.
+    status = Wait_To_Receive(card);
+    if (status != HAL_OK) return status;
+
+    uint8_t CSD_reg[18];
+
+    status = SD_SPI_Receive_Data(card->spi_handle, CSD_reg, sizeof(CSD_reg));
+    if (status != HAL_OK) return status;
+
+
+    // check the crc16 
+    // calculate crc7 using generator polynomial (pattern).
+    uint16_t crc16_bytes;
+    uint64_t data = (((uint64_t)cmd << 32) | (uint64_t)cmd_arg);
+    uint64_t pattern = (uint64_t)0x89 << 39; // divisor 
+    uint64_t data_dividend = data << 7;
+    for (uint8_t i = 46; i >= 7; i--) {
+        if ((data_dividend & ((uint64_t)1 << i)) != 0) {
+            data_dividend ^= pattern;
+        }
+        pattern >>= 1;
+    } 
+    crc7_byte = ((uint8_t)(data_dividend & (uint64_t)0x7f) << 1) | 1;
+
+
+
+    // verify that it is a High Capacity or Extended Capacity Card. 
+    if ((CSD_reg[0] & 0xc0) != 0x40) {
+        return HAL_ERROR;
+    }
+
+    uint32_t c_size = (((uint8_t)CSD_reg[7] & 0x3f) << 16) | ((uint8_t)CSD_reg[8] << 8) | (uint8_t)CSD_reg[9];
+
+}
+
 
 /** This init function first resets the card into SPI mode, than validates the
     card, waits for the card to be ready, 
